43859b1927ce6d9f902a512a853eb7678f63c63c
epersia5 <30569471+epersia5@users.noreply.github.com>
2022-09-23 18:46:40 +0330

    Add files via upload

---
+++ build/precache-manifest.ed9f921f7736babeb49b46a20d6af6c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1acba2e70cf7e0962cc2",
+    "url": "/robofriends/static/css/main.ae4c991b.chunk.css"
+  },
+  {
+    "revision": "1acba2e70cf7e0962cc2",
+    "url": "/robofriends/static/js/main.80e9b1b3.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "475dd1385a30875df2af",
+    "url": "/robofriends/static/css/2.3ffe45e1.chunk.css"
+  },
+  {
+    "revision": "475dd1385a30875df2af",
+    "url": "/robofriends/static/js/2.cf92c1d9.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3d068214f254c98eaa1b0bcb045fa89a",
+    "url": "/robofriends/index.html"
+  }
+];
